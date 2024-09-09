086d669db13774ee89a7a2086b873bec1c887d87
marleo <mariole@edu.aau.at>
2024-09-09 14:01:30 +0200

    - added local-config-example.js

---
+++ backend/local-config-example.js
@@ -0,0 +1,7 @@
+class LocalConfig {
+    static config_CLIP_SERVER = 'localhost:8002'
+    static config_MONGODB_SERVER = 'localhost:27017'; 
+    static config_MONGODB = 'esopxplore';
+}
+
+module.exports = LocalConfig;
